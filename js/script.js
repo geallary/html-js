46e43456cb12ba95eeff495faaa90f7e5a05aab4
geallary <geoffrey.allary.pro@gmail.com>
2017-09-14 14:00:29 +0200

    third commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
+$(document).ready(init);
+function init(){
 $("h1").click(gererclick);
 function gererclick(){
-    console.log("çamarche");
-}
+    var colorR = Math.floor((Math.random() * 256));
+    var colorG = Math.floor((Math.random() * 256));
+    var colorB = Math.floor((Math.random() * 256));
+    console.log(this);
+    $(this).css("color","rgb(" + colorR + "," + colorG + "," + colorB + ")");
+    console.log($(this).text());
+}}
+
